1526ed91766e3b9c403ee17d8df47a4de5595aee
trandvinh <92655794+trandvinh@users.noreply.github.com>
2021-11-09 11:21:44 -0600

    Add files via upload

---
+++ DevCruxClassApp/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using DevCruxClassApp.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace DevCruxClassApp.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
